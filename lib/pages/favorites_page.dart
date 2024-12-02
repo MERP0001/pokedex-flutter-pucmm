526bfe221bcba8a2de3324aad8bd24e21ca46a94
ArmandoGonzalez <ajgl0001@ce.pucmm.edu.do>
2024-12-02 13:39:00 -0400

    Mejorar visuales de lista de favoritos

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'pokemon_detail_page.dart';
-import 'Pokemon.dart'; // Asegúrate de importar el modelo Pokemon
+import 'Pokemon.dart';
 
 class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
@@ -13,6 +13,26 @@ class FavoritesPage extends StatefulWidget {
 
 class _FavoritesPageState extends State<FavoritesPage> {
   List<int> favoritePokemonIds = [];
+  final Map<String, Color> typeColors = {
+    'fire': Colors.red,
+    'water': Colors.blue,
+    'grass': Colors.green,
+    'electric': Colors.yellow,
+    'normal': Colors.grey,
+    'fighting': Colors.brown,
+    'flying': Colors.lightBlueAccent,
+    'poison': Colors.purple,
+    'ground': Colors.orange,
+    'rock': Colors.brown[700]!,
+    'bug': Colors.lightGreen,
+    'ghost': Colors.deepPurple,
+    'steel': Colors.blueGrey,
+    'psychic': Colors.pink,
+    'ice': Colors.cyan,
+    'dragon': Colors.indigo,
+    'dark': Colors.black,
+    'fairy': Colors.pinkAccent,
+  };
 
   @override
   void initState() {
@@ -66,67 +86,154 @@ class _FavoritesPageState extends State<FavoritesPage> {
       appBar: AppBar(
         title: const Text('Pokémon Favoritos'),
       ),
-      body: Query(
-        options: QueryOptions(
-          document: gql(query),
-          variables: {'ids': favoritePokemonIds},
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.lightBlueAccent,
+          image: DecorationImage(
+            image: const AssetImage('assets/icons/pokeball.png'),
+            fit: BoxFit.none,
+            alignment: Alignment.center,
+            colorFilter: ColorFilter.mode(
+              Colors.white.withOpacity(0.1),
+              BlendMode.dstATop,
+            ),
+          ),
         ),
-        builder: (QueryResult result, {fetchMore, refetch}) {
-          if (result.hasException) {
-            return Center(child: Text(result.exception.toString()));
-          }
+        child: Query(
+          options: QueryOptions(
+            document: gql(query),
+            variables: {'ids': favoritePokemonIds},
+          ),
+          builder: (QueryResult result, {fetchMore, refetch}) {
+            if (result.hasException) {
+              return Center(child: Text(result.exception.toString()));
+            }
 
-          if (result.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          }
+            if (result.isLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
 
-          final pokemonsData =
-              result.data?['pokemon_v2_pokemon'] as List<dynamic>? ?? [];
-          final favoritePokemons = pokemonsData.map((pokemonData) {
-            return Pokemon(
-              id: pokemonData['id'],
-              name: pokemonData['name'],
-              types: (pokemonData['pokemon_v2_pokemontypes'] as List)
-                  .map((type) => type['pokemon_v2_type']['name'] as String)
-                  .toList(),
-              generation: pokemonData['pokemon_v2_pokemonspecy']
-                  ['pokemon_v2_generation']['name'],
-              height: 0.0,
-              weight: 0.0,
-              abilities: [],
-              stats: [],
-              moves: [],
-            );
-          }).toList();
-
-          return ListView.builder(
-            itemCount: favoritePokemons.length,
-            itemBuilder: (context, index) {
-              final pokemon = favoritePokemons[index];
-              final primaryType =
-                  pokemon.types.isNotEmpty ? pokemon.types[0] : 'normal';
-              const color =
-                  Colors.grey; // Puedes usar un mapa de colores similar
-              final imageUrl =
-                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png';
-
-              return ListTile(
-                leading: Image.network(imageUrl),
-                title: Text('#${pokemon.id} ${pokemon.name}'),
-                subtitle: Text(pokemon.generation),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          PokemonDetailPage(pokemonId: pokemon.id),
-                    ),
-                  );
-                },
+            final pokemonsData =
+                result.data?['pokemon_v2_pokemon'] as List<dynamic>? ?? [];
+            final favoritePokemons = pokemonsData.map((pokemonData) {
+              return Pokemon(
+                id: pokemonData['id'],
+                name: pokemonData['name'],
+                types: (pokemonData['pokemon_v2_pokemontypes'] as List)
+                    .map((type) => type['pokemon_v2_type']['name'] as String)
+                    .toList(),
+                generation: pokemonData['pokemon_v2_pokemonspecy']
+                    ['pokemon_v2_generation']['name'],
+                height: 0.0,
+                weight: 0.0,
+                abilities: [],
+                stats: [],
+                moves: [],
               );
-            },
-          );
-        },
+            }).toList();
+
+            return ListView.builder(
+              itemCount: favoritePokemons.length,
+              itemBuilder: (context, index) {
+                final pokemon = favoritePokemons[index];
+                final primaryType =
+                    pokemon.types.isNotEmpty ? pokemon.types[0] : 'normal';
+                final color = typeColors[primaryType] ?? Colors.grey;
+                final imageUrl =
+                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png';
+
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            PokemonDetailPage(pokemonId: pokemon.id),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12.0, horizontal: 16.0),
+                      decoration: BoxDecoration(
+                        color: color.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.4),
+                            blurRadius: 8,
+                            spreadRadius: 2,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                        image: DecorationImage(
+                          image: const AssetImage('assets/icons/pokeball.png'),
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                            Colors.white.withOpacity(0.2),
+                            BlendMode.dstATop,
+                          ),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Image.network(
+                            imageUrl,
+                            width: 80,
+                            height: 80,
+                            fit: BoxFit.contain,
+                          ),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '#${pokemon.id} ${pokemon.name}',
+                                  style: const TextStyle(
+                                    fontFamily: 'DiaryOfAn8BitMage',
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Row(
+                                  children: pokemon.types.map((type) {
+                                    return Padding(
+                                      padding:
+                                          const EdgeInsets.only(right: 8.0),
+                                      child: Image.asset(
+                                        'assets/icons/$type.png',
+                                        width: 32,
+                                        height: 32,
+                                      ),
+                                    );
+                                  }).toList(),
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  pokemon.generation,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'DiaryOfAn8BitMage',
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
